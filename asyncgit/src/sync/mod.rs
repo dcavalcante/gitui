497010a1f5a9f9266b8d2e5a933c957d9fa31266
Daniel Cavalcante <dcavalcante@gmail.com>
2026-09-06 00:50:03 -0300

    feat(asyncgit): export checkout methods

--- asyncgit/src/sync/mod.rs
+++ asyncgit/src/sync/mod.rs
@@ -5,6 +5,7 @@
 
 pub mod blame;
 pub mod branch;
+mod checkout;
 pub mod commit;
 mod commit_details;
 pub mod commit_files;
@@ -45,6 +46,7 @@ pub use branch::{
 	merge_rebase::merge_upstream_rebase, rename::rename_branch,
 	validate_branch_name, BranchCompare, BranchDetails, BranchInfo,
 };
+pub use checkout::{checkout_with_method, CheckoutMethod};
 pub use commit::{amend, commit, tag_commit};
 pub use commit_details::{
 	get_commit_details, CommitDetails, CommitMessage, CommitSignature,
